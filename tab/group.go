523c96bfc753320c3b0986504860424f44a00dc1
Michal Kubista <kubistmi@gmail.com>
2020-10-22 00:08:15 +0200

    Implement the Aggregation method

--- tab/group.go
+++ tab/group.go
@@ -1,5 +1,7 @@
 package tab
 
+import "github.com/kubistmi/goframe/vec"
+
 // GetIndex ...
 func (df Table) GetGroups() map[int][]int {
 	return df.index.grp
@@ -42,5 +44,57 @@ func (df Table) Group(cols []string) Table {
 	}{cols, nix}
 
 	return (df)
+}
 
+// Agg ...
+func (df Table) Agg(mapf map[string]interface{}) Table {
+
+	cols := make([]string, 0, len(mapf))
+	for c := range mapf {
+		cols = append(cols, c)
+	}
+	if err := df.checkCols(cols); err != nil {
+		return Table{err: err}
+	}
+
+	new := make(map[string]vec.Vector, len(mapf)+len(df.index.cols))
+
+	grps := make([]int, 0, len(df.index.grp))
+	ix := make([]int, 0, len(grps))
+	for grp, rs := range df.index.grp {
+		grps = append(grps, grp)
+		ix = append(ix, rs[0])
+	}
+
+	dfgrp := df.Cols(df.index.cols).Rows(ix)
+	for _, n := range dfgrp.names {
+		new[n] = dfgrp.data[n]
+	}
+
+	cols = make([]string, len(df.index.cols), len(mapf)+len(df.index.cols))
+	copy(cols, df.index.cols)
+	for col, fun := range mapf {
+		switch f := fun.(type) {
+		case func(vec.Vector) int:
+			aggval := make([]int, len(grps))
+			for i, g := range grps {
+				aggval[i] = f(df.data[col].Loc(df.index.grp[g]))
+			}
+			new[col] = vec.NewVec(aggval)
+			cols = append(cols, col)
+		case func(...vec.Vector) string:
+			aggval := make([]string, len(grps))
+			for i, g := range grps {
+				aggval[i] = f(df.data[col].Loc(df.index.grp[g]))
+			}
+			new[col] = vec.NewVec(aggval)
+			cols = append(cols, col)
+		}
+	}
+
+	return Table{
+		data:  new,
+		names: cols,
+		size:  [2]int{len(grps), len(cols)},
+	}
 }
--- tab/group_test.go
+++ tab/group_test.go
@@ -1,11 +1,21 @@
 package tab
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/kubistmi/goframe/vec"
 )
+
+func meanAge(v vec.Vector) int {
+	vals, _ := v.(vec.IntVector).Get()
+	sum := 0
+	for _, x := range vals {
+		sum = sum + x
+	}
+	return sum / len(vals)
+}
 
 func TestGroup(t *testing.T) {
 	df, _ := NewDf(map[string]vec.Vector{
@@ -13,11 +23,13 @@ func TestGroup(t *testing.T) {
 		"sex":   vec.NewVec([]string{"m", "f", "f", "f", "m", "f", "m", "m"}),
 		"group": vec.NewVec([]int{1, 0, 2, 2, 0, 2, 1, 1}),
 	})
-	got := df.Group([]string{"sex", "group"}).GetGroups()
+	grDf := df.Group([]string{"sex", "group"})
+	got := grDf.GetGroups()
 	want := map[int][]int{0: []int{0, 6, 7}, 2: []int{4}, 3: []int{1}, 5: []int{2, 3, 5}}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Expected: %#v, got: %#v", want, got)
 	}
 
+	fmt.Println(grDf.Agg(map[string]interface{}{"age": meanAge}))
 }
