b70df37058a10d3c434b273319e596a22986a237
Michal Kubista <kubistmi@gmail.com>
2021-02-28 17:30:07 +0100

    Helper function Seq

--- vec/utils.go
+++ vec/utils.go
@@ -32,3 +32,17 @@ func Isin(w interface{}) interface{} {
 	return fmt.Errorf("wrong parameter type, expected: []int/[]string, got: %T", w)
 
 }
+
+func Seq(from, to, by int) []int {
+
+	if (by > 0 && from < to) || (by < 0 && from > to) {
+		size := (to - from) / by
+		out := make([]int, size)
+		for ix := range out {
+			out[ix] = from + ix*by
+		}
+		return out
+	}
+
+	return []int{}
+}
