6c837d0e15f8539fbc48ad8fc208c5a86adf7a3b
Michal Kubista <kubistmi@gmail.com>
2021-02-28 17:31:32 +0100

    Sorting now handles NA (considered as the smallest values)

--- vec/sort.go
+++ vec/sort.go
@@ -7,18 +7,34 @@ import (
 type ordering struct {
 	s  sort.Interface
 	ix []int
+	na Set
 }
 
-func (o ordering) Less(i, j int) bool { return o.s.Less(o.ix[i], o.ix[j]) }
-func (o ordering) Len() int           { return o.s.Len() }
-func (o ordering) Swap(i, j int)      { o.ix[i], o.ix[j] = o.ix[j], o.ix[i] }
+func (o ordering) Less(i, j int) bool {
+	return o.s.Less(o.ix[i], o.ix[j]) || (o.na.Get(i) && !o.na.Get(j))
+}
+func (o ordering) Len() int { return o.s.Len() }
 
-func order(s sort.Interface) []int {
+func (o ordering) Swap(i, j int) {
+	nai := o.na.Get(i)
+	naj := o.na.Get(j)
+	if nai && !naj {
+		o.na = o.na.Del(i)
+		o.na = o.na.Set(j)
+	}
+	if naj && !nai {
+		o.na = o.na.Del(j)
+		o.na = o.na.Set(i)
+	}
+	o.ix[i], o.ix[j] = o.ix[j], o.ix[i]
+}
+
+func order(s sort.Interface, na Set) []int {
 	pos := make([]int, s.Len())
 	for ix := range pos {
 		pos[ix] = ix
 	}
-	sort.Stable(ordering{s: s, ix: pos})
+	sort.Stable(ordering{s: s, ix: pos, na: na})
 	return pos
 }
 
@@ -42,10 +58,10 @@ func (v StrVector) Sort() Vector {
 
 // Order ...
 func (v IntVector) Order() []int {
-	return order(sort.IntSlice(v.obs))
+	return order(sort.IntSlice(v.obs), v.na)
 }
 
 // Order ...
 func (v StrVector) Order() []int {
-	return order(sort.StringSlice(v.obs))
+	return order(sort.StringSlice(v.obs), v.na)
 }
